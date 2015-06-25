c286e5fc333bb1196a4a626fe54b00acd0747b91
leorising14 <cufarog@hotmail.com>
2015-06-25 00:44:10 -0300

    Cuadricula
    Se le agrega la cuadricula

--- main.c
+++ main.c
@@ -17,21 +17,18 @@ const float FPS = 60;
 const int SCREEN_W = 1000;
 const int SCREEN_H = 700;
 const int IMG_SIZE = 92;
-
+const int BUTTON_SIZE_W = 250;
 
 int main(int argc, char **argv){
  
    ALLEGRO_DISPLAY *display = NULL;
-   ALLEGRO_BITMAP  *estado1   = NULL;
-   ALLEGRO_BITMAP *nuevoestado = NULL;
+   ALLEGRO_BITMAP *nuevoestado = NULL;                      //creo los punteros a las imagenes correspondientes a los botones
    ALLEGRO_BITMAP *nuevatransicion = NULL;
    ALLEGRO_BITMAP *nuevafuncion = NULL;
    ALLEGRO_BITMAP *makefile = NULL;
    ALLEGRO_BITMAP *blankspace = NULL;
    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
    ALLEGRO_TIMER *timer = NULL;
-   float estado1_x = SCREEN_W / 2.0 - IMG_SIZE / 2.0;
-   float estado1_y = SCREEN_H / 2.0 - IMG_SIZE / 2.0;//lo hago aparecer en el medio
    bool key[4] = { false, false, false, false };
    bool redraw = true;
    bool doexit = false;
@@ -43,16 +40,8 @@ int main(int argc, char **argv){
    int var=0;
    int n=0;
    int contadordeestados=0;
-   miestado* listadeestados;
-   listadeestados = NULL;
-   //miestado newestado1;
-   //newestado1.estado_x = SCREEN_W / 2.0 - IMG_SIZE / 2.0;
-   //newestado1.estado_y = SCREEN_H / 2.0 - IMG_SIZE / 2.0;
+   miestado* listadeestados = NULL;
 
-   //listadeestados = &newestado1;
-   
-   
-   
    if(!al_init()) {
       al_show_native_message_box(display, "Error", "Error", "No se pudo inicializar allegro!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
@@ -88,8 +77,7 @@ int main(int argc, char **argv){
       return 0;
    }
  
-  // newestado1.estadoimg = al_load_bitmap("estado1.png");
-   nuevoestado = al_load_bitmap("nuevoestado.png");
+   nuevoestado = al_load_bitmap("nuevoestado.png");                 //cargo las imágenes de los botones correspondientes
    nuevafuncion = al_load_bitmap("nuevafuncion.png");
    nuevatransicion = al_load_bitmap("nuevatransicion.png");
    makefile = al_load_bitmap("makefile.png");
@@ -112,9 +100,9 @@ int main(int argc, char **argv){
    al_set_target_bitmap(al_get_backbuffer(display));
    
    event_queue = al_create_event_queue();
+   
    if(!event_queue) {
       fprintf(stderr, "failed to create event_queue!\n");
-      al_destroy_bitmap(estado1);
       al_destroy_display(display);
       al_destroy_timer(timer);
       return -1;
@@ -127,7 +115,8 @@ int main(int argc, char **argv){
    al_register_event_source(event_queue, al_get_keyboard_event_source()); //meti los tres eventos en la misma cola
 
    al_register_event_source(event_queue, al_get_mouse_event_source());
-    al_init_primitives_addon();
+    
+   al_init_primitives_addon();
 
    al_clear_to_color(al_map_rgb(255,255,255));
  
@@ -148,10 +137,10 @@ int main(int argc, char **argv){
       }
       else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
        
-          for(n=1;n<contadordeestados;n++){
-       if(((ev.mouse.x)<(leerestado(n,listadeestados)->estado_x+92)) && (((ev.mouse.y)<(leerestado(n,listadeestados)->estado_y+92))) 
+       for(n=0;n<contadordeestados;n++){
+            if(((ev.mouse.x)<(leerestado(n,listadeestados)->estado_x+92)) && (((ev.mouse.y)<(leerestado(n,listadeestados)->estado_y+92))) 
                && ((leerestado(n,listadeestados)->estado_x)<ev.mouse.x) && ((leerestado(n,listadeestados)->estado_y)<ev.mouse.y)){
-            mousestate = 1;
+                mousestate = 1;
        }
        }
        if(((ev.mouse.x)<(725+250)) && (((ev.mouse.y)<(150+54))) && ((725)<ev.mouse.x) && ((150)<ev.mouse.y)){
@@ -170,7 +159,7 @@ int main(int argc, char **argv){
           mousestate = 0;
       }else if((mousestate == 1)){
           //printf("El mouse esta en: %d\n",ev.mouse.x);        //Para pruebas
-          for(n=1;n<contadordeestados;n++){
+          for(n=0;n<contadordeestados;n++){
           leerestado(n,listadeestados)->estado_x = (ev.mouse.x)-46;
           leerestado(n,listadeestados)->estado_y = (ev.mouse.y)-46;
           }
@@ -183,18 +172,17 @@ int main(int argc, char **argv){
       }else if((newstate == 1)){
           printf("Agregue estado\n");
           var=1;
-          contadordeestados++;
-          agregarestado(&listadeestados);
+
           agregarestado(&listadeestados);
           (leerestado(contadordeestados,listadeestados))->estado_x = SCREEN_W / 2.0 - IMG_SIZE / 2.0;
           (leerestado(contadordeestados,listadeestados))->estado_y = SCREEN_H / 2.0 - IMG_SIZE / 2.0;
           (leerestado(contadordeestados,listadeestados))->estadoimg = al_load_bitmap("estado1.png");
-          
+          contadordeestados++;
+
           //for(n=0;n<contadordeestados;n++){
           //  leerestado(n,listadeestados)->estadoimg = al_load_bitmap("estado1.png");      
          // }
 
-          al_draw_bitmap(blankspace, 725, 250, 0);
           newstate=0;
       }else if((newtransicion == 1)){
           printf("Aregue transicion\n");
@@ -208,7 +196,7 @@ int main(int argc, char **argv){
          al_clear_to_color(al_map_rgb(255,255,255));
          if(var==1)
          {
-             for(n=1;n<contadordeestados;n++){
+             for(n=0;n<contadordeestados;n++){
                 al_draw_bitmap(leerestado(n,listadeestados)->estadoimg, leerestado(n,listadeestados)->estado_x, leerestado(n,listadeestados)->estado_y, 0);
              } 
          }
@@ -217,14 +205,23 @@ int main(int argc, char **argv){
          al_draw_bitmap(nuevafuncion, 725, 350, 0);
          al_draw_bitmap(makefile, 725, 500, 0);
          
-         al_draw_line(estado1_x,estado1_y,40,50,al_map_rgb(0,255,0),2);
- 
+         for(n=(IMG_SIZE)/2;n<(SCREEN_W-BUTTON_SIZE_W);n=n+IMG_SIZE)
+         {
+           al_draw_line(n,0,n,7.5*IMG_SIZE,al_map_rgb(0,255,0),2);             
+         }
+         
+         for(n=(IMG_SIZE)/2;n<SCREEN_H;n=n+IMG_SIZE)
+         {
+           al_draw_line(0,n,7.5*IMG_SIZE,n,al_map_rgb(0,255,0),2);             
+         }
+
          al_flip_display();
       }
    }
 
-   
-   al_destroy_bitmap(estado1);
+   for(n=0;n<contadordeestados;n++){
+        al_destroy_bitmap(leerestado(n,listadeestados)->estadoimg);
+   }
    al_destroy_timer(timer);
    al_destroy_display(display);
    al_destroy_event_queue(event_queue);
