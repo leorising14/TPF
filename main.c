58e9050a88f905f69b277f1c126461e338c00f22
leorising14 <cufarog@hotmail.com>
2015-06-25 16:08:04 -0300

    Botones de borrado
    Se agregan los botones de borrado

--- main.c
+++ main.c
@@ -18,6 +18,15 @@ const int SCREEN_W = 1000;
 const int SCREEN_H = 700;
 const int IMG_SIZE = 92;
 const int BUTTON_SIZE_W = 250;
+const int BUTTON_SIZE_H = 54;
+const int BUTTONS_COLUMN = 725;
+const int BUTTON1_FILE = 50;
+const int BUTTON2_FILE = 130;
+const int BUTTON3_FILE = 210;
+const int BUTTON4_FILE = 300;
+const int BUTTON5_FILE = 380;
+const int BUTTON6_FILE = 460;
+const int BUTTON_MAKEFILE_FILE = 570;
 
 int main(int argc, char **argv){
  
@@ -25,6 +34,9 @@ int main(int argc, char **argv){
    ALLEGRO_BITMAP *nuevoestado = NULL;                      //creo los punteros a las imagenes correspondientes a los botones
    ALLEGRO_BITMAP *nuevatransicion = NULL;
    ALLEGRO_BITMAP *nuevafuncion = NULL;
+   ALLEGRO_BITMAP *borrarestado = NULL;
+   ALLEGRO_BITMAP *borrartransicion = NULL;
+   ALLEGRO_BITMAP *borrarfuncion = NULL;
    ALLEGRO_BITMAP *makefile = NULL;
    ALLEGRO_BITMAP *blankspace = NULL;
    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
@@ -81,22 +93,20 @@ int main(int argc, char **argv){
    nuevoestado = al_load_bitmap("nuevoestado.png");                 //cargo las imágenes de los botones correspondientes
    nuevafuncion = al_load_bitmap("nuevafuncion.png");
    nuevatransicion = al_load_bitmap("nuevatransicion.png");
+   borrarestado = al_load_bitmap("borrarestado.png");
+   borrarfuncion = al_load_bitmap("borrarfuncion.png");
+   borrartransicion = al_load_bitmap("borrartransicion.png");
    makefile = al_load_bitmap("makefile.png");
    blankspace = al_load_bitmap("blankspace.png");
    
-   if(!nuevoestado || !nuevafuncion || !nuevatransicion || !makefile) {
+   if(!nuevoestado || !nuevafuncion || !nuevatransicion || !borrarestado || !borrarfuncion || !borrartransicion || !makefile) {
       fprintf(stderr, "No se pudieron crear los bitmaps!\n");
       al_destroy_display(display);
       al_destroy_timer(timer);
       return -1;
    }
   
-   //if(!newestado1.estadoimg) {
-    //  fprintf(stderr, "No se pudieron crear los bitmaps!\n");
-    //  al_destroy_display(display);
-    //  al_destroy_timer(timer);
-    //  return -1;
-  // }
+
    
    al_set_target_bitmap(al_get_backbuffer(display));
    
@@ -139,21 +149,21 @@ int main(int argc, char **argv){
       else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
        
        for(n=0;n<contadordeestados;n++){
-            if(((ev.mouse.x)<(leerestado(n,listadeestados)->estado_x+92)) && (((ev.mouse.y)<(leerestado(n,listadeestados)->estado_y+92))) 
+            if(((ev.mouse.x)<(leerestado(n,listadeestados)->estado_x+IMG_SIZE)) && (((ev.mouse.y)<(leerestado(n,listadeestados)->estado_y+IMG_SIZE))) 
                && ((leerestado(n,listadeestados)->estado_x)<ev.mouse.x) && ((leerestado(n,listadeestados)->estado_y)<ev.mouse.y)){
                 mousestate = 1;
                 estadoactual = n;
             }
        }
-       if(((ev.mouse.x)<(725+250)) && (((ev.mouse.y)<(150+54))) && ((725)<ev.mouse.x) && ((150)<ev.mouse.y)){
+       if(((ev.mouse.x)<(BUTTONS_COLUMN+BUTTON_SIZE_W)) && (((ev.mouse.y)<(BUTTON1_FILE+BUTTON_SIZE_H))) && ((BUTTONS_COLUMN)<ev.mouse.x) && ((BUTTON1_FILE)<ev.mouse.y)){
             newstate = 1;
        }
        
-       if(((ev.mouse.x)<(725+250)) && (((ev.mouse.y)<(250+54))) && ((725)<ev.mouse.x) && ((250)<ev.mouse.y)){
+       if(((ev.mouse.x)<(BUTTONS_COLUMN+BUTTON_SIZE_W)) && (((ev.mouse.y)<(BUTTON2_FILE+BUTTON_SIZE_H))) && ((BUTTONS_COLUMN)<ev.mouse.x) && ((BUTTON2_FILE)<ev.mouse.y)){
             newtransicion = 1;
        }
        
-       if(((ev.mouse.x)<(725+250)) && (((ev.mouse.y)<(350+54))) && ((725)<ev.mouse.x) && ((350)<ev.mouse.y)){
+       if(((ev.mouse.x)<(BUTTONS_COLUMN+BUTTON_SIZE_W)) && (((ev.mouse.y)<(BUTTON3_FILE+BUTTON_SIZE_H))) && ((BUTTONS_COLUMN)<ev.mouse.x) && ((BUTTON3_FILE)<ev.mouse.y)){
             newfunction = 1;
        }
        
@@ -162,8 +172,8 @@ int main(int argc, char **argv){
       }else if((mousestate == 1)){
           //printf("El mouse esta en: %d\n",ev.mouse.x);        //Para pruebas
           
-          leerestado(estadoactual,listadeestados)->estado_x = (ev.mouse.x)-46;
-          leerestado(estadoactual,listadeestados)->estado_y = (ev.mouse.y)-46;
+          leerestado(estadoactual,listadeestados)->estado_x = (ev.mouse.x)-IMG_SIZE/2;
+          leerestado(estadoactual,listadeestados)->estado_y = (ev.mouse.y)-IMG_SIZE/2;
 
       }else if(ev.type == ALLEGRO_EVENT_KEY_UP){
           switch(ev.keyboard.keycode) {
@@ -179,13 +189,16 @@ int main(int argc, char **argv){
           (leerestado(contadordeestados,listadeestados))->estado_x = SCREEN_W / 2.0 - IMG_SIZE / 2.0;
           (leerestado(contadordeestados,listadeestados))->estado_y = SCREEN_H / 2.0 - IMG_SIZE / 2.0;
           (leerestado(contadordeestados,listadeestados))->estadoimg = al_load_bitmap("estado1.png");
+
+          if(!(leerestado(contadordeestados,listadeestados))->estadoimg){
+            fprintf(stderr, "No se pudo crear el bitmap!\n");
+            al_destroy_display(display);
+            al_destroy_timer(timer);
+            return -1;
+          }
+          
           contadordeestados++;
-
-          //for(n=0;n<contadordeestados;n++){
-          //  leerestado(n,listadeestados)->estadoimg = al_load_bitmap("estado1.png");      
-         // }
-
-          newstate=0;
+          newstate=0;          
       }else if((newtransicion == 1)){
           printf("Aregue transicion\n");
       }else if((newfunction == 1)){
@@ -202,10 +215,13 @@ int main(int argc, char **argv){
                 al_draw_bitmap(leerestado(n,listadeestados)->estadoimg, leerestado(n,listadeestados)->estado_x, leerestado(n,listadeestados)->estado_y, 0);
              } 
          }
-         al_draw_bitmap(nuevoestado, 725, 150, 0);
-         al_draw_bitmap(nuevatransicion, 725, 250, 0);
-         al_draw_bitmap(nuevafuncion, 725, 350, 0);
-         al_draw_bitmap(makefile, 725, 500, 0);
+         al_draw_bitmap(nuevoestado, BUTTONS_COLUMN, BUTTON1_FILE, 0);
+         al_draw_bitmap(nuevatransicion, BUTTONS_COLUMN, BUTTON2_FILE, 0);
+         al_draw_bitmap(nuevafuncion, BUTTONS_COLUMN, BUTTON3_FILE, 0);
+         al_draw_bitmap(borrarestado, BUTTONS_COLUMN, BUTTON4_FILE, 0);
+         al_draw_bitmap(borrartransicion, BUTTONS_COLUMN, BUTTON5_FILE, 0);
+         al_draw_bitmap(borrarfuncion, BUTTONS_COLUMN, BUTTON6_FILE, 0);         
+         al_draw_bitmap(makefile, BUTTONS_COLUMN, BUTTON_MAKEFILE_FILE, 0);
          
          for(n=(IMG_SIZE)/2;n<(SCREEN_W-BUTTON_SIZE_W);n=n+IMG_SIZE)
          {
